e71467b77147678c6878c6570b0feddfa41dbf3d
Fabian.Jakub <Fabian.Jakub@402f7b55-a32c-4b4d-9b89-0138b0a842ac>
2014-04-03 10:05:24 +0000

    added compile script for different architectures - and reenabled march=native and mtune=native for RELEASE
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/hdcp2-rt/tenstream@321 402f7b55-a32c-4b4d-9b89-0138b0a842ac

--- src/optprop_dir1_diff2/boxmc_parameters.f90
+++ src/optprop_dir1_diff2/boxmc_parameters.f90
@@ -7,7 +7,7 @@ module boxmc_parameters_1_2
 
       real(ireals),parameter :: delta_scale_truncate=.9962_ireals ! .9962 = 5 degrees delta scaling
 
-      integer(iintegers) ,parameter :: Ndz=10, Nkabs=40, Nksca=40, Ng=10, Nphi=2, Ntheta=10, interp_mode=2 
+      integer(iintegers) ,parameter :: Ndz=20, Nkabs=40, Nksca=40, Ng=15, Nphi=2, Ntheta=10, interp_mode=2 
 !      integer(iintegers) ,parameter :: Ndz=5, Nkabs=5, Nksca=5, Ng=5, Nphi=2, Ntheta=10, interp_mode=2 
 !      integer(iintegers) ,parameter :: Ndz=2, Nkabs=2, Nksca=2, Ng=2, Nphi=2, Ntheta=10, interp_mode=2 
 
