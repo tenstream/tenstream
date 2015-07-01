f2165b9c245513b8e8cc56f41329e4f0330062f9
Max.Eckl <Max.Eckl@physik.uni-muenchen.de>
2015-07-01 10:42:26 +0200

    inital commit for ann stuff

--- src/optprop_parameters.f90
+++ src/optprop_parameters.f90
@@ -46,7 +46,7 @@ module m_optprop_parameters
       !!        this assumes you actually have a
       !!        ANN that can be queried....
       !! 
-      integer(iintegers),parameter :: coeff_mode = 0
+      integer(iintegers),parameter :: coeff_mode = 1
 
       !-----------------------------------------
       !- Define the size of the Lookuptables:  -
