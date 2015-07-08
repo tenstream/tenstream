f1350435074cb948f48e16c3a313705fa8624d21
Fabian Jakub <fabian@jakub.com>
2015-07-08 13:50:32 +0200

    update: change mistral config file to use AVX2 optimizations and be compatible to Mellanox cards compilation process

--- config/mistral.cmake
+++ config/mistral.cmake
@@ -1,25 +1,35 @@
 # Mistral supercomputer at DKRZ Hamburg
 # modules loaded:
 #   module load intel intelmpi
+#   export MPIMODE=impi
+#   export MYmpif90=mpiifort
+#   export MYmpicc=mpiicc
+#   export MYmpicxx=mpiicpc
+
+# or with mellanox support
 #   module load intel mxm fca bullxmpi_mlx
+#   export MPIMODE=bullxmpi
+#   export MYmpif90=mpif90
+#   export MYmpicc=mpicc
+#   export MYmpicxx=mpicxx
 
 # Netcdf paths taken from nc-config // nf-config
 #
-# export NETCDFCROOT = /sw/rhel6-x64/netcdf/netcdf_c-4.3.2-parallel-bullxmpi-intel14/      
-# export NETCDFFROOT = /sw/rhel6-x64/netcdf/netcdf_fortran-4.4.2-parallel-bullxmpi-intel14/
-# export HDF5ROOT    = /sw/rhel6-x64/hdf5/hdf5-1.8.14-parallel-bullxmpi-intel14/
+# export NETCDFCROOT = /sw/rhel6-x64/netcdf/netcdf_c-4.3.2-parallel-$MPIMODE-intel14/      
+# export NETCDFFROOT = /sw/rhel6-x64/netcdf/netcdf_fortran-4.4.2-parallel-$MPIMODE-intel14/
+# export HDF5ROOT    = /sw/rhel6-x64/hdf5/hdf5-1.8.14-parallel-$MPIMODE-intel14/
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NETCDFFROOT/lib:$NETCDFCROOT/lib:
 
 set(NETCDF_DIR      "$ENV{NETCDFCROOT}")
 set(NETCDF_DIR_F90  "$ENV{NETCDFFROOT}")
 set(HDF5_DIR        "$ENV{HDF5ROOT}")
 
 # Petsc installed with:
-# export PETSC_ARCH=debug
+# export PETSC_ARCH=fast-$MPIMODE
 #./configure                           \
-#  --prefix="$PETSC_PREFIX/$PETSC_ARCH"\
-#  --with-cc=$(which mpicc)            \
-#  --with-fc=$(which mpif90)           \
-#  --with-cxx=$(which mpicxx)          \
+#  --with-cc=$(which  $MYmpicc)        \
+#  --with-fc=$(which  $MYmpif90)       \
+#  --with-cxx=$(which $MYmpicxx)       \
 #  --with-shared-libraries=0           \
 #  --with-netcdf-dir=$NETCDFCROOT      \
 #  --with-hdf5-dir=$HDF5ROOT           \
@@ -31,16 +41,16 @@ set(HDF5_DIR        "$ENV{HDF5ROOT}")
 #  --download-hypre                    \
 #  --with-debugging=0                  \
 #  --with-valgrind-dir=/usr            \
-#  COPTFLAGS='-O3 -no-prec-div -xAVX -mkl -fno-omit-frame-pointer' \
-#  FOPTFLAGS='-O3 -no-prec-div -xAVX -mkl -fno-omit-frame-pointer' \
+#  COPTFLAGS='-O3 -xAVX -mkl' \
+#  FOPTFLAGS='-O3 -xAVX -mkl' \
 #  \
 #  && make all test install
 
-set(CMAKE_C_COMPILER "mpicc")
-set(CMAKE_CXX_COMPILER "mpicxx")
-set(CMAKE_Fortran_COMPILER "mpif90")
+set(CMAKE_C_COMPILER       "$ENV{MYmpicc}")
+set(CMAKE_CXX_COMPILER     "$ENV{MYmpicxx}")
+set(CMAKE_Fortran_COMPILER "$ENV{MYmpif90}")
 
 set(USER_C_FLAGS "-std=c99")
 set(USER_Fortran_FLAGS "-cpp -traceback -extend_source -g -sox ")
-set(USER_Fortran_FLAGS_RELEASE " -O3 -no-prec-div -xAVX -fp-model source -fno-omit-frame-pointer ")
+set(USER_Fortran_FLAGS_RELEASE " -O3 -xCORE-AVX2 -fp-model source ")
 set(USER_Fortran_FLAGS_DEBUG "-fpe0 -O0 -g -check all -check nopointers -check noarg_temp_created")
