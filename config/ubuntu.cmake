035035da03d8eb4c0e03e42deb3b596ed0dd187a
Fabian Jakub <fabian@jakub.com>
2016-07-08 02:35:37 +0200

    dont run range checks in debug mode

--- config/ubuntu.cmake
+++ config/ubuntu.cmake
@@ -10,6 +10,6 @@ set(CMAKE_Fortran_COMPILER   "/usr/bin/mpif90")
 set(Fortran_COMPILER_WRAPPER "/usr/bin/mpif90")
 
 set(USER_C_FLAGS               "-cpp -W -Wall -Wuninitialized --std=c99") 
-set(USER_Fortran_FLAGS         "-cpp -ffree-line-length-none -W -Wall -Wuninitialized -g") 
+set(USER_Fortran_FLAGS         "-cpp -ffree-line-length-none -W -Wall -g")
 set(USER_Fortran_FLAGS_RELEASE "-fno-backtrace -fno-range-check -O3") 
-set(USER_Fortran_FLAGS_DEBUG   "-fbacktrace -finit-real=nan -W -Wall -Wuninitialized -g -pg -fcheck=all -fbounds-check -pedantic -Wsurprising")
+set(USER_Fortran_FLAGS_DEBUG   "-fbacktrace -finit-real=nan -W -Wall -Wuninitialized -g -pg -fcheck=all -fbounds-check -pedantic -Wsurprising -fno-range-check")
