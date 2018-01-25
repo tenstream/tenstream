46c8ddac1e66e126d9a16119ae736d259bfcacf8
Fabian.Jakub <fabian@jakub.com>
2018-01-25 19:19:02 +0100

    introduce new function to load data from netcdf file and broadcast it into dmplex ordering

--- plexrt/plex_ex2.F90
+++ plexrt/plex_ex2.F90
@@ -14,10 +14,12 @@ use m_icon_grid, only: t_icongrid, read_icon_grid_file, &
 
 use m_plex_grid, only: t_plexgrid, create_plex_from_icongrid, &
   setup_edir_dmplex, setup_abso_dmplex, compute_face_geometry, &
-  distribute_plexgrid_dm
+  distribute_plexgrid_dm, ncvar2d_to_globalvec
 
 use m_plex_rt, only: get_normal_of_first_toa_face, create_src_vec, &
   create_edir_mat, solve_plex_rt, compute_edir_absorption
+
+use m_netcdfio, only : ncload
 
 
 implicit none
@@ -34,12 +36,15 @@ logical, parameter :: ldebug=.True.
 
       integer(mpiint) :: myid, numnodes, ierr
       type(t_plexgrid), allocatable :: plexgrid
-      type(tVec) :: b, edir, abso
+      type(tVec) :: b, edir, abso, lwcvec
       type(tMat) :: A
       AO :: cell_ao
 
-      integer(iintegers), parameter :: Nz=10
+      integer(iintegers), parameter :: Nz=2
       real(ireals) :: sundir(3) ! cartesian direction of sun rays in a global reference system
+      real(ireals),allocatable :: hhl(:)
+
+      call ncload(['lwc.nc      ', 'height_level'], hhl, ierr)
 
       call mpi_comm_rank(comm, myid, ierr); call CHKERR(ierr)
       call mpi_comm_size(comm, numnodes, ierr); call CHKERR(ierr)
@@ -52,15 +57,18 @@ logical, parameter :: ldebug=.True.
       call distribute_icon_grid(comm, icongrid, local_icongrid, cell_ao)
       deallocate(icongrid)
 
-      call create_plex_from_icongrid(comm, Nz, local_icongrid, plexgrid)
+      call create_plex_from_icongrid(comm, Nz, hhl, local_icongrid, plexgrid)
       plexgrid%cell_ao = cell_ao
       deallocate(local_icongrid)
+
+      call ncvar2d_to_globalvec(plexgrid, 'lwc.nc', 'lwc', lwcvec)
+      call PetscObjectViewFromOptions(lwcvec, PETSC_NULL_VEC, '-show_lwc', ierr); call CHKERR(ierr)
 
       call compute_face_geometry(plexgrid) ! setup the geometry info for the plexgrid object (sets up the plexgrid%geom_dm)
 
       call setup_edir_dmplex(plexgrid, plexgrid%edir_dm)
 
-      sundir = get_normal_of_first_TOA_face(plexgrid) + [0.,0.,-.1]
+      sundir = get_normal_of_first_TOA_face(plexgrid) + [0.,0.,-.3]
       sundir = sundir/norm(sundir)
       print *,myid,'Initial sundirection = ', sundir
 
@@ -75,8 +83,8 @@ logical, parameter :: ldebug=.True.
       call PetscObjectViewFromOptions(edir, PETSC_NULL_VEC, '-show_edir', ierr); call CHKERR(ierr)
 
       call compute_edir_absorption(plexgrid, edir, sundir, abso)
-
     end subroutine
+
   end module
 
   program main
@@ -89,12 +97,20 @@ logical, parameter :: ldebug=.True.
     character(len=default_str_len) :: default_options
     logical :: lexists
 
-    default_options='-show_abso hdf5:plex_ex2_out.h5::append'
-    inquire( file='plex_ex2_out.h5', exist=lexists )
-    if(.not.lexists) default_options = '-show_plex hdf5:plex_ex2_out.h5 '//trim(default_options)
+    default_options=''
+    default_options=trim(default_options)//' -show_abso hdf5:plex_ex2_out.h5::append'
+    default_options=trim(default_options)//' -show_ownership hdf5:plex_ex2_out.h5::append'
+    default_options=trim(default_options)//' -show_iconindex hdf5:plex_ex2_out.h5::append'
+    default_options=trim(default_options)//' -show_zindex hdf5:plex_ex2_out.h5::append'
+    default_options=trim(default_options)//' -show_lwc hdf5:plex_ex2_out.h5::append'
 
     call PetscInitialize(PETSC_NULL_CHARACTER,ierr); call CHKERR(ierr)
     call init_mpi_data_parameters(PETSC_COMM_WORLD)
+
+    call mpi_barrier(PETSC_COMM_WORLD, ierr); call CHKERR(ierr)
+    inquire( file='plex_ex2_out.h5', exist=lexists )
+    if(.not.lexists) default_options = '-show_plex hdf5:plex_ex2_out.h5 '//trim(default_options)
+    call mpi_barrier(PETSC_COMM_WORLD, ierr); call CHKERR(ierr)
 
     print *,'Adding default Petsc Options:', default_options, lexists
     call PetscOptionsInsertString(PETSC_NULL_OPTIONS, default_options, ierr)
