2658773dec014f84b178e0ae91109b0f2d63567e
Fabian Jakub <fabian@jakub.com>
2018-03-22 01:38:00 +0100

    update mistral cmake config

--- config/mistral.cmake
+++ config/mistral.cmake
@@ -1,24 +1,28 @@
 # Mistral supercomputer at DKRZ Hamburg
 # modules loaded:
-#   module load intel intelmpi
-#   export MPIMODE=impi
-#   export MYmpif90=mpiifort
-#   export MYmpicc=mpiicc
-#   export MYmpicxx=mpiicpc
 
-# or with mellanox support
-#   module load intel mxm fca bullxmpi_mlx
-#   export MPIMODE=bullxmpi
-#   export MYmpif90=mpif90
-#   export MYmpicc=mpicc
-#   export MYmpicxx=mpicxx
-
-# Netcdf paths taken from nc-config // nf-config
+# module add intel/16.0.2 mxm/3.3.3002 fca/2.5.2393 bullxmpi_mlx/bullxmpi_mlx-1.2.8.3
 #
-# export NETCDFCROOT = /sw/rhel6-x64/netcdf/netcdf_c-4.3.2-parallel-$MPIMODE-intel14/      
-# export NETCDFFROOT = /sw/rhel6-x64/netcdf/netcdf_fortran-4.4.2-parallel-$MPIMODE-intel14/
-# export HDF5ROOT    = /sw/rhel6-x64/hdf5/hdf5-1.8.14-parallel-$MPIMODE-intel14/
+# export OMPI_MCA_pml=cm         # sets the point-to-point management layer
+# export OMPI_MCA_mtl=mxm        # sets the matching transport layer (MPI-2 one-sided comm.)
+# export MXM_RDMA_PORTS=mlx5_0:1
+# export OMPI_MCA_coll=^ghc             # disable BULLs GHC algorithm for collectives
+# export OMPI_MCA_coll_fca_priority=95
+# export OMPI_MCA_coll_fca_enable=1
+#
+# export MPIMODE=bullxmpi
+# export MYmpif90=mpif90
+# export MYmpicc=mpicc
+# export MYmpicxx=mpicxx
+#
+# # NETCDF LIBPATH
+# export NETCDFCROOT=/sw/rhel6-x64/netcdf/netcdf_c-4.3.2-parallel-$MPIMODE-intel14/
+# export NETCDFFROOT=/sw/rhel6-x64/netcdf/netcdf_fortran-4.4.2-parallel-$MPIMODE-intel14/
+# export HDF5ROOT=/sw/rhel6-x64/hdf5/hdf5-1.8.14-parallel-$MPIMODE-intel14/
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NETCDFFROOT/lib:$NETCDFCROOT/lib:
+# export PATH=$PATH:$NETCDFFROOT/bin:$NETCDFCROOT/bin
+# 
+# module load cmake
 
 set(NETCDF_DIR      "$ENV{NETCDFCROOT}")
 set(NETCDF_DIR_F90  "$ENV{NETCDFFROOT}")
@@ -37,13 +41,13 @@ set(HDF5_DIR        "$ENV{HDF5ROOT}")
 #  --with-fortran                      \
 #  --with-fortran-interfaces           \
 #  --with-cmake=$(which cmake)         \
-#  --download-ml                       \
-#  --download-hypre                    \
+#  --with-precision=single             \
+#  --download-metis                    \
+#  --download-parmetis                 \
 #  --with-debugging=0                  \
 #  --with-valgrind-dir=/usr            \
 #  COPTFLAGS='-O3 -xAVX -mkl' \
 #  FOPTFLAGS='-O3 -xAVX -mkl' \
-#  \
 #  && make all test install
 
 set(CMAKE_C_COMPILER       "$ENV{MYmpicc}")
