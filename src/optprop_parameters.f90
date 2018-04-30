97ed2f2c8110211dcbeec5d89145b2c1d27faa60
Fabian.Jakub <fabian@jakub.com>
2018-04-30 14:13:15 +0200

    set stddev_atol=2e-4_ireals as a default, checking for smaller values doesnt work in single precision

--- src/optprop_parameters.f90
+++ src/optprop_parameters.f90
@@ -91,8 +91,8 @@ module m_optprop_parameters
 
 !      real(ireals),parameter :: stddev_atol=5e-3_ireals
 !      real(ireals),parameter :: stddev_atol=1e-3_ireals
-      real(ireals),parameter :: stddev_atol=5e-4_ireals
-!      real(ireals),parameter :: stddev_atol=1e-4_ireals
+!      real(ireals),parameter :: stddev_atol=5e-4_ireals
+      real(ireals),parameter :: stddev_atol=2e-4_ireals
 !      real(ireals),parameter :: stddev_atol=5e-6_ireals
 
       real(ireals),parameter :: stddev_rtol=1e-2_ireals
