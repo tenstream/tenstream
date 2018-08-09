7fef93641b35c4dcc154565712d79f17bf7960ce
Fabian.Jakub <fabian@jakub.com>
2018-08-09 23:04:33 +0200

    remove adding_delta_twostream. Some valgrind errors hit me but I dont want to investigate now..

--- src/pprts.f90
+++ src/pprts.f90
@@ -2097,7 +2097,14 @@ module m_pprts
         if(allocated(atm%planck) ) then
           call delta_eddington_twostream(dtau,w0,g,mu0,incSolar,atm%albedo(i,j), S,Edn,Eup, planck=atm%planck(:,i,j) )
         else
-          call adding_delta_eddington_twostream(dtau,w0,g,mu0,incSolar,atm%albedo(i,j), S,Edn,Eup )
+          !
+          ! call adding_delta_eddington_twostream(dtau,w0,g,mu0,incSolar,atm%albedo(i,j), S,Edn,Eup )
+          !
+          !TODO investigate if this one is really ok...
+          ! I recently had valgrind errors in VecNorm after calling this:
+          ! make -j ex_pprts_rrtm_lw_sw &&
+          ! mpirun -np 1 -wdir ../examples/rrtm_lw_sw/ valgrind $(pwd)/bin/ex_pprts_rrtm_lw_sw -twostr_only
+          call delta_eddington_twostream(dtau,w0,g,mu0,incSolar,atm%albedo(i,j), S,Edn,Eup )
         endif
 
         if(solution%lsolar_rad) then
