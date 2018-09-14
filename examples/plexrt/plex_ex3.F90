2d052c54eceeb03cf45da152a0e8714837b490f7
Fabian.Jakub <fabian@jakub.com>
2018-09-14 15:45:32 +0200

    add option to plex_ex3 to switch thermal computations on and off and add edir to print output

--- examples/plexrt/plex_ex3.F90
+++ examples/plexrt/plex_ex3.F90
@@ -6,7 +6,7 @@ use petsc
 use m_tenstream_options, only : read_commandline_options
 
 use m_helper_functions, only: CHKERR, norm, imp_bcast, determine_normal_direction, &
-  spherical_2_cartesian, angle_between_two_vec, rad2deg
+  spherical_2_cartesian, angle_between_two_vec, rad2deg, reverse
 
 use m_data_parameters, only : ireals, iintegers, mpiint, &
   default_str_len, &
@@ -56,7 +56,9 @@ logical, parameter :: ldebug=.True.
       integer(iintegers), allocatable :: zindex(:)
       class(t_plex_solver), allocatable :: solver
 
-      real(ireals), allocatable, dimension(:,:) :: edn, eup, abso
+      real(ireals), allocatable, dimension(:,:) :: edir, edn, eup, abso
+
+      logical, parameter :: lthermal = .False.
 
       call mpi_comm_rank(comm, myid, ierr); call CHKERR(ierr)
       call mpi_comm_size(comm, numnodes, ierr); call CHKERR(ierr)
@@ -65,8 +67,9 @@ logical, parameter :: ldebug=.True.
 
       hhl(1) = zero
       do k=2,Nz
-        hhl(k) = hhl(k-1) - dz
+        hhl(k) = hhl(k-1) + dz
       enddo
+      hhl = reverse(hhl)
 
       call dmplex_2D_to_3D(dm2d, hhl, dm3d, zindex)
 
@@ -89,49 +92,46 @@ logical, parameter :: ldebug=.True.
       call init_plex_rt_solver(plex, solver)
 
       first_normal = get_normal_of_first_TOA_face(solver%plex)
-      !sundir = first_normal + [zero, -.001_ireals, zero]
-      !sundir = -[0.688915, -0.422213, 0.589179]
-      sundir = -[0.1674986798086554, -0.2833506736583789, 0.9442756949110258]
-      !!sundir = -[0.677688, 0.0758756, 0.731425]
-      !!sundir = -[0.826811, 0.0269913, 0.561832]
-      !!sundir = -[0.775165, 0.0535335, 0.629487] ! sza 10deg
-      !!sundir = -[0.72362, 0.0793532, 0.685621]
-      !!sundir = -[0.717145, 0.0262298, 0.696431] !further left
-      !sundir = -[0.717607, -0.690197, -0.0930992]
-      !sundir = -[0.88119, -0.0874145, 0.46461]
+      sundir = first_normal !+ [zero, -.00001_ireals, zero]
+
       sundir = sundir/norm(sundir)
       print *,myid,'Initial sundirection = ', sundir, rad2deg(angle_between_two_vec(sundir, first_normal))
 
       !!call set_plex_rt_optprop(solver, vlwc=lwcvec, viwc=iwcvec)
       call set_plex_rt_optprop(solver)
+
+
       if(.not.allocated(solver%albedo)) then
         allocate(solver%albedo)
         call DMCreateGlobalVector(solver%plex%srfc_boundary_dm, solver%albedo, ierr); call CHKERR(ierr)
       endif
       call VecSet(solver%albedo, Ag, ierr); call CHKERR(ierr)
 
-      if(.not.allocated(solver%plck)) then
-        allocate(solver%plck)
-        call DMCreateGlobalVector(solver%plex%cell1_dm, solver%plck, ierr); call CHKERR(ierr)
+      if(lthermal) then
+        if(.not.allocated(solver%plck)) then
+          allocate(solver%plck)
+          call DMCreateGlobalVector(solver%plex%cell1_dm, solver%plck, ierr); call CHKERR(ierr)
+        endif
+        call VecSet(solver%plck, 100._ireals, ierr); call CHKERR(ierr)
+
+        if(.not.allocated(solver%srfc_emission)) then
+          allocate(solver%srfc_emission)
+          call DMCreateGlobalVector(solver%plex%srfc_boundary_dm, solver%srfc_emission, ierr); call CHKERR(ierr)
+        endif
+        call VecSet(solver%srfc_emission, 400._ireals/3.1415_ireals, ierr); call CHKERR(ierr)
+        call run_plex_rt_solver(solver, lthermal=.True., lsolar=.False., sundir=sundir)
+      else
+        call run_plex_rt_solver(solver, lthermal=.False., lsolar=.True., sundir=sundir)
       endif
-      call VecSet(solver%plck, 100._ireals, ierr); call CHKERR(ierr)
 
-      if(.not.allocated(solver%srfc_emission)) then
-        allocate(solver%srfc_emission)
-        call DMCreateGlobalVector(solver%plex%srfc_boundary_dm, solver%srfc_emission, ierr); call CHKERR(ierr)
-      endif
-      call VecSet(solver%srfc_emission, 400._ireals/3.1415_ireals, ierr); call CHKERR(ierr)
-
-      call run_plex_rt_solver(solver, lthermal=.True., lsolar=.False., sundir=sundir)
-
-      call plexrt_get_result(solver, edn, eup, abso)
+      call plexrt_get_result(solver, edn, eup, abso, redir=edir)
       call destroy_plexrt_solver(solver, lfinalizepetsc=.False.)
 
-      print *,'k       Edn       Eup          abso'
+      print *,'k                Edir                Edn                          Eup                          abso'
       do k = 1, ubound(abso,1)
-        print *,k, edn(k,1), eup(k,1), abso(k,1)
+        print *,k, edir(k,1), edn(k,1), eup(k,1), abso(k,1)
       enddo
-      print *,k, edn(k,1), eup(k,1)
+      print *,k, edir(k,1), edn(k,1), eup(k,1)
     end subroutine
 
   end module
