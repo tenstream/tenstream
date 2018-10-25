8e7e286c9d815492f98dd2e77c3dd6f4e2c82161
Fabian.Jakub <fabian@jakub.com>
2018-10-25 12:27:41 +0200

    minor tweaks to python solve script

--- misc/solve_binary_dump.py
+++ misc/solve_binary_dump.py
@@ -4,38 +4,48 @@ petsc4py.init(sys.argv)
 
 from petsc4py import PETSc as P
 import numpy as np
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+myid = comm.Get_rank()
 
 OptDB = P.Options()
 fname_mat = OptDB.getString('-mat', 'mat.bin')
 fname_b   = OptDB.getString('-b', 'b.bin')
 
-print("Loading Matrix File: {}".format(fname_mat))
+check_matrows = OptDB.getBool('-check_matrows', False)
+
+if myid==0:
+    print("Loading Matrix File: {}".format(fname_mat))
 
 viewer = P.Viewer().createBinary(fname_mat, 'r')
 A = P.Mat().load(viewer)
 A = A.setUp()
-rStart, rEnd = A.getOwnershipRange()
-for i in range(rStart, rEnd):
-    col_idx, coeff = A.getRow(i)
-    if not np.isfinite(coeff).all():
-        print("Have NaN in mat coeffs", coeff)
+if check_matrows:
+    rStart, rEnd = A.getOwnershipRange()
+    for i in range(rStart, rEnd):
+        col_idx, coeff = A.getRow(i)
+        if not np.isfinite(coeff).all():
+            print("Have NaN in mat coeffs", coeff)
 
-    if any(coeff>1.):
-        print("Have coeffs gt 1", coeff)
+        if any(coeff>1.):
+            print("Have coeffs gt 1", coeff)
 
-    if any(coeff<-1):
-        print("Have coeffs lt -1", coeff)
+        if any(coeff<-1):
+            print("Have coeffs lt -1", coeff)
 
-    cv = A.getColumnVector(i)
-    if np.sum(cv.array)<0:
-        print("Have column vector with sum lt 1: icol", i, np.where(cv.array!=0), \
-                ':', cv.array[cv.array!=0]), 'sum', np.sum(cv.array)
+        cv = A.getColumnVector(i)
+        if np.sum(cv.array)<0:
+            print("Have column vector with sum lt 1: icol", i, np.where(cv.array!=0), \
+                    ':', cv.array[cv.array!=0]), 'sum', np.sum(cv.array)
 
+if myid==0:
+    print("Loading src Vec: {}".format(fname_b))
 viewer = P.Viewer().createBinary(fname_b, 'r')
 b = P.Vec().load(viewer)
 b = b.setUp()
 if not np.isfinite(b.array).all():
-    print("Have NaN in src term")
+    print(myid,"Have NaN in src term")
 
 x = b.duplicate()
 
@@ -44,9 +54,8 @@ ksp.setOperators(A)
 ksp.setFromOptions()
 ksp.solve(b, x)
 
-fname_plot = None
-fname_plot = OptDB.getString('-plot')
-if fname_plot is not None:
+fname_plot = OptDB.getString('-plot', '')
+if fname_plot is not '':
     from pylab import *
     Ncells = 27
     Nlevel = 179
