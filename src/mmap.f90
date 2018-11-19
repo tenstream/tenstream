af83d15046846bfd4e06a16bed8763904f1385e9
Fabian.Jakub <fabian@jakub.com>
2018-11-19 23:11:18 +0100

    add a header in front of mmaps including byte size of values, the total array size, total arraysize in bytes and the dimensions

--- src/mmap.f90
+++ src/mmap.f90
@@ -1,20 +1,24 @@
 module m_mmap
-use iso_c_binding
+  use iso_fortran_env, only: INT64
+  use iso_c_binding
 
-use m_data_parameters, only : iintegers, irealLUT, mpiint
-use m_helper_functions, only : CHKERR, imp_bcast, itoa
-use m_netcdfIO, only: acquire_file_lock, release_file_lock
+  use m_data_parameters, only : iintegers, irealLUT, mpiint
+  use m_helper_functions, only : CHKERR, imp_bcast, itoa, resize_arr
+  use m_netcdfIO, only: acquire_file_lock, release_file_lock
 
-implicit none
-! Definitions from mman-linux.h
+  implicit none
+  !Posix Standard Definition for sysconf
+  integer, parameter :: &
+    SC_PAGESIZE = 30
 
-integer, parameter :: &
-  PROT_READWRITE = 3, &
-  PROT_READ = 1,      &
-  MAP_SHARED = 1,     &
-  MAP_PRIVATE = 2,    &
-  MAP_NORESERVE = 16384, & ! X'04000'
-  O_RDONLY = 0
+  ! Definitions from mman-linux.h
+  integer, parameter :: &
+    PROT_READWRITE = 3, &
+    PROT_READ = 1,      &
+    MAP_SHARED = 1,     &
+    MAP_PRIVATE = 2,    &
+    MAP_NORESERVE = 16384, & ! X'04000'
+    O_RDONLY = 0
 
 interface
   type(c_ptr) function c_mmap(addr, length, prot, &
@@ -48,7 +52,17 @@ interface
     integer(c_int), value :: fd
   end function
 end interface
+interface
+  integer(c_long) function c_sysconf(name) bind(c,name='sysconf')
+    use iso_c_binding
+    integer(c_int), value :: name
+  end function
+end interface
 
+
+interface load_mmap_array
+  module procedure load_mmap_array_2d
+end interface
 interface arr_to_binary_datafile
   module procedure arr_to_binary_datafile_2d
 end interface
@@ -63,39 +77,101 @@ contains
     integer :: funit, flock_unit
     logical :: lexists
 
+    integer(c_size_t), allocatable :: header(:)
+    integer(c_size_t), parameter :: bytesize_header=c_sizeof(bytesize_header)
+    integer(c_long) :: c_pagesize
+
+    integer(c_size_t) :: size_of_inp_arr, bytesize
+    integer(c_size_t), parameter :: dtype_size=c_sizeof(arr(1,1))
+
+    size_of_inp_arr = size(arr, kind=c_size_t)
+    bytesize = dtype_size * size_of_inp_arr
+
+    c_pagesize = c_sysconf(SC_PAGESIZE)
+
+    allocate(header(c_pagesize/bytesize_header))
+    header(:) = 0
+    header(1) = dtype_size
+    header(2) = size_of_inp_arr
+    header(3) = bytesize
+    header(4) = size(arr,dim=1)
+    header(5) = size(arr,dim=2)
+
     call acquire_file_lock(fname, flock_unit, ierr); call CHKERR(ierr)
     ierr = 0
     inquire(file=trim(fname), exist=lexists)
     if(lexists) then
-      call release_file_lock(flock_unit, ierr); call CHKERR(ierr)
-      return
       open(newunit=funit, file=trim(fname), form='unformatted', access='stream', status='replace')
     else
       open(newunit=funit, file=trim(fname), form='unformatted', access='stream', status='new')
     endif
-    write (unit=funit) arr
+    write (unit=funit) header, arr
     close(funit)
     call release_file_lock(flock_unit, ierr); call CHKERR(ierr)
   end subroutine
 
-  subroutine binary_file_to_mmap(fname, bytesize, mmap_c_ptr)
+  subroutine binary_file_to_mmap(fname, mmap_c_ptr, dtype_size, arr_shape)
     character(len=*), intent(in) :: fname
-    integer(c_size_t), intent(in) :: bytesize
     type(c_ptr), intent(out) :: mmap_c_ptr
-    integer(c_size_t),parameter :: offset=0
+    integer(c_size_t), intent(out) :: dtype_size
+    integer(c_size_t), allocatable, intent(out), optional :: arr_shape(:)
+    integer(c_size_t) :: offset
 
+    integer :: funit, i
     integer(c_int) :: c_fd
 
     logical :: lexists
 
+    integer(c_size_t) :: bytesize_data
+    integer(c_long) :: c_pagesize
+    integer(c_size_t), allocatable :: header(:)
+    integer(c_size_t), parameter :: bytesize_header=c_sizeof(bytesize_header)
+
+    c_pagesize = c_sysconf(SC_PAGESIZE)
+    offset = c_pagesize
+
     inquire(file=trim(fname), exist=lexists)
     if(.not.lexists) call CHKERR(1_mpiint, 'Tried to create a mmap from a file that does not exist: '//fname)
 
+    allocate(header(c_pagesize/bytesize_header))
+
+    open(newunit=funit, file=trim(fname), form='unformatted', access='stream', status='old')
+    read (unit=funit) header
+    close(funit)
+    dtype_size = header(1)
+    bytesize_data = header(3)
+
+    if(present(arr_shape)) then
+      allocate(arr_shape(count(header(4:size(header)).ne.0)))
+      do i = 1, size(arr_shape)
+        arr_shape(i) = header(3+i)
+      enddo
+    endif
+
     c_fd = c_open(trim(fname)//C_NULL_CHAR, O_RDONLY)
     if(c_fd.le.0) call CHKERR(c_fd, 'Could not open mmap file')
-    mmap_c_ptr = c_mmap(0_c_int, bytesize, PROT_READ, IOR(MAP_PRIVATE, MAP_NORESERVE), c_fd, offset)
+    mmap_c_ptr = c_mmap(0_c_int, bytesize_data, PROT_READ, IOR(MAP_PRIVATE, MAP_NORESERVE), c_fd, offset)
     c_fd = c_close(c_fd)
     if(c_fd.le.0) call CHKERR(c_fd, 'Could not close file descriptor to mmap file')
+  end subroutine
+
+  subroutine load_mmap_array_2d(fname, arr)
+    character(len=*), intent(in) :: fname
+    real(irealLUT), pointer, intent(out) :: arr(:,:)
+
+    type(c_ptr) :: mmap_c_ptr
+    integer(c_size_t), allocatable :: arrshape(:)
+    integer(c_size_t) :: dtype_size
+
+    call binary_file_to_mmap(fname, mmap_c_ptr, dtype_size, arrshape)
+
+    if(dtype_size.ne.c_sizeof(arr(1,1))) &
+      call CHKERR(1_mpiint, 'size type of binary data '//itoa(dtype_size)//'does not match inp_arr_dtype'//itoa(c_sizeof(arr(1,1))))
+
+    if(size(arrshape).ne.2) &
+      call CHKERR(size(arrshape), 'mmap in '//trim(fname)//' has '//itoa(size(arrshape))//' dimensions... expected 2')
+
+    call c_f_pointer(mmap_c_ptr, arr, arrshape)
   end subroutine
 
   subroutine arr_to_mmap(comm, fname, mmap_ptr, ierr, inp_arr)
@@ -109,9 +185,8 @@ contains
     integer(iintegers), allocatable :: arrshape(:)
     type(c_ptr) :: mmap_c_ptr
     integer(mpiint) :: myid
-    integer(c_size_t) :: size_of_inp_arr
-    integer(c_size_t) :: dtype_size
-    integer(c_size_t) :: bytesize
+    integer(c_size_t), parameter :: dtype_size=c_sizeof(1._irealLUT) ! size of the supplied irealLUT type
+    integer(c_size_t) :: size_of_inp_arr, dtype_size_mmap_data, bytesize
 
     call mpi_comm_rank(comm, myid, ierr); call CHKERR(ierr)
 
@@ -121,7 +196,6 @@ contains
       if(.not.present(inp_arr)) call CHKERR(1_mpiint, 'rank 0 has to provide an input array!')
       call arr_to_binary_datafile(inp_arr, fname_fpsuffix, ierr)
       if(ierr.ne.0) print *,'arr_to_mmap::binary file already exists, I did not overwrite it... YOU have to make sure that the file is as expected or delete it...'
-      dtype_size = c_sizeof(inp_arr(1,1))
       size_of_inp_arr = size(inp_arr, kind=c_size_t)
       bytesize = dtype_size * size_of_inp_arr
       if(size_of_inp_arr.ge.huge(size_of_inp_arr)/dtype_size) then
@@ -143,7 +217,10 @@ contains
       call CHKERR(1_mpiint, 'bytesize of mmap is wrong!'//itoa(int(bytesize,iintegers)))
     endif
 
-    call binary_file_to_mmap(fname_fpsuffix, bytesize, mmap_c_ptr)
+    call binary_file_to_mmap(fname_fpsuffix, mmap_c_ptr, dtype_size_mmap_data)
+    if(dtype_size.ne.dtype_size_mmap_data) &
+      call CHKERR(1_mpiint, 'size type of binary data '//itoa(dtype_size)// &
+        ' does not match inp_arr_dtype '//itoa(dtype_size_mmap_data))
 
     call c_f_pointer(mmap_c_ptr, mmap_ptr, arrshape)
   end subroutine
@@ -162,8 +239,7 @@ contains
 
     mmap_ptr=>NULL()
 
-    cerr = c_munmap(mmap_c_ptr, bytesize)
-    ierr = cerr
+    cerr = c_munmap(mmap_c_ptr, bytesize); ierr = cerr
     call CHKERR(ierr, 'Error Unmapping the memory map')
   end subroutine
 
--- tests/test_mmap/test_mmap.f90
+++ tests/test_mmap/test_mmap.f90
@@ -1,6 +1,6 @@
 module test_mmap
     use m_data_parameters, only: irealLUT, iintegers, mpiint, init_mpi_data_parameters
-    use m_mmap, only: arr_to_mmap, munmap_mmap_ptr
+    use m_mmap, only: arr_to_mmap, munmap_mmap_ptr, arr_to_binary_datafile, binary_file_to_mmap, load_mmap_array
 
     use m_helper_functions, only: CHKERR
 
@@ -11,7 +11,7 @@ module test_mmap
   contains
 
 @test(npes =[2,1])
-subroutine test_mpi_functions(this)
+subroutine test_mpi_write_and_read_mmaps(this)
     class (MpiTestMethod), intent(inout) :: this
 
     integer(iintegers), parameter :: N=3
@@ -54,9 +54,56 @@ subroutine test_mpi_functions(this)
       enddo
     enddo
 
-    call munmap_mmap_ptr(arr, ierr)
-    call CHKERR(ierr, 'Err unmapping mmap')
+    call munmap_mmap_ptr(arr, ierr); call CHKERR(ierr, 'Err unmapping mmap')
     print *,'Testing mmap with ', numnodes,'ranks ... done'
+end subroutine
+
+@test(npes =[2,1])
+subroutine test_write_read_mmap(this)
+    class (MpiTestMethod), intent(inout) :: this
+
+    integer(iintegers), parameter :: N=5
+    character(len=*), parameter :: fname='pfunit_test.mmap'
+
+    integer(iintegers) :: i, j
+    integer(mpiint) :: numnodes, comm, myid, ierr
+
+    real(irealLUT), pointer :: arr(:,:)
+    real(irealLUT), pointer :: mmap_ptr(:,:)
+
+    arr=>NULL()
+    mmap_ptr=>NULL()
+
+    comm     = this%getMpiCommunicator()
+    numnodes = this%getNumProcesses()
+    myid     = this%getProcessRank()
+
+    print *,'Testing mmap with ', numnodes,'ranks ...'
+
+    call init_mpi_data_parameters(comm)
+
+    if(myid.eq.0) then
+      allocate(arr(N,N))
+      do i = 1, N
+        do j = 1, N
+          arr(i,j) = real((i-1)*N+j, irealLUT)
+        enddo
+      enddo
+      call arr_to_binary_datafile(arr, fname, ierr); call CHKERR(ierr)
+    endif
+
+    call mpi_barrier(comm, ierr); call CHKERR(ierr)
+
+
+    call load_mmap_array(fname, mmap_ptr)
+
+    do i = 1, N
+      do j = 1, N
+        @assertEqual(real((i-1)*N+j, irealLUT), mmap_ptr(i,j))
+      enddo
+    enddo
+
+    call munmap_mmap_ptr(mmap_ptr, ierr); call CHKERR(ierr, 'Err unmapping mmap')
 end subroutine
 
 end module
