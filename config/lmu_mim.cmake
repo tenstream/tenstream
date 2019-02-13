07ca1c535d61c2ecb27c7024af87a746aa579495
Fabian.Jakub <fabian@jakub.com>
2019-02-13 16:08:34 +0100

    remove bad compiler flag for gcc

--- config/lmu_mim.cmake
+++ config/lmu_mim.cmake
@@ -15,7 +15,7 @@ set(Fortran_COMPILER_WRAPPER "mpif90")
 
 set(USER_C_FLAGS               "-cpp -W -std=c99")
 set(USER_Fortran_FLAGS         "-cpp -ffree-line-length-none -g")
-set(USER_Fortran_FLAGS_RELEASE "-fno-range-check -O3 -march=native -mtune=native -xHost")
+set(USER_Fortran_FLAGS_RELEASE "-fno-range-check -O3 -march=native -mtune=native")
 set(USER_Fortran_FLAGS_DEBUG   "-fbacktrace -finit-real=nan -Werror -Wall -pedantic -g -pg -fcheck=all -fbounds-check -ffpe-trap=invalid")
 
 set(NETCDF_DIR      "$ENV{NETCDF}")
