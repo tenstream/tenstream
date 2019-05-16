9eb74a372b659380f4010c4ae865f369a69e20dd
Fabian.Jakub <fabian@jakub.com>
2019-05-16 12:33:02 +0200

    renaming code files from .f90 to .F90 to account for the default meaning of preprocessable src files

--- config/lmu_mim.cmake
+++ config/lmu_mim.cmake
@@ -13,8 +13,8 @@ set(CMAKE_C_COMPILER   "mpicc")
 set(CMAKE_Fortran_COMPILER   "mpif90")
 set(Fortran_COMPILER_WRAPPER "mpif90")
 
-set(USER_C_FLAGS               "-cpp -W -std=c99")
-set(USER_Fortran_FLAGS         "-cpp -ffree-line-length-none -g")
+set(USER_C_FLAGS               "-W -std=c99")
+set(USER_Fortran_FLAGS         "-ffree-line-length-none -g")
 set(USER_Fortran_FLAGS_RELEASE "-fno-range-check -O3 -mtune=native")
 set(USER_Fortran_FLAGS_DEBUG   "-O0 -fbacktrace -finit-real=nan -Werror -Wall -pedantic -g -pg -fcheck=all -fbounds-check -ffpe-trap=invalid")
 
