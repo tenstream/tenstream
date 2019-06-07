a92614f0866c574f4387752542915e4593392ace
Fabian.Jakub <fabian@jakub.com>
2019-06-07 20:41:34 +0200

    add a little script to run a couple of plexrt solvers and time them

---
+++ misc/plexrt_benchmark/run_tests.sh
@@ -0,0 +1,6 @@
+for dx in 100 500
+do
+  bash run_solver_tests.sh thermal_diff False True $dx
+  bash run_solver_tests.sh solar_dir    True False $dx
+  bash run_solver_tests.sh solar_diff   True False $dx
+done
