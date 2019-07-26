6afffcd47d10688ee6a2e2f33ec943a42f7185f5
Fabian.Jakub <fabian@jakub.com>
2019-07-26 17:22:53 +0200

    fix the lrz config and add an option to the cmake petsc finder to skip tests because the cmake c_code runtests deadlock hang in MPI_Init for single threaded applications. we just have to believe in petsc working

--- config/lrz.cmake
+++ config/lrz.cmake
@@ -1,13 +1,12 @@
 # LRZ Linux Cluster
 # before, load modules:
 #
-# module unload mkl mpi.intel intel
+# module purge
 #
-# module load intel
-# module load mpi.intel
-# module load mkl
-# module load netcdf/4.6.1-intel-impi-hdf5v1.10-parallel
-# module load netcdf-fortran/4.4.4-intel-impi-hdf5v1.10
+# module load lrz/nodev cmake git
+# module load intel/19.1
+# module load mpi.intel/2019
+# module load mkl/2019_s
 
 set(CMAKE_C_COMPILER   "mpiicc")
 set(CMAKE_Fortran_COMPILER   "mpiifort")
@@ -15,10 +14,11 @@ set(Fortran_COMPILER_WRAPPER "mpiifort")
 
 set(USER_C_FLAGS "-std=c99")
 set(USER_Fortran_FLAGS "-cpp -traceback -extend_source -g")
-set(USER_Fortran_FLAGS_RELEASE " -O3 -no-prec-div -fp-model source ")
+set(USER_Fortran_FLAGS_RELEASE "-march=native -O3 -ftz -fp-model fast=2 -no-prec-div -no-prec-sqrt -fast-transcendentals")
 set(USER_Fortran_FLAGS_DEBUG "-fpe0 -O0 -g -check all -check nopointers -check noarg_temp_created")
 
-set(USER_MPIEXEC "srun")
+set(USER_MPIEXEC "mpiexec")
+set(PETSC_SKIP_TEST_RUNS True)
 
-set(NETCDF_DIR "$ENV{NETCDF_BASE}")
-set(NETCDF_DIR_F90 "$ENV{NETCDF_FORTRAN_BASE}")
+set(NETCDF_DIR "$ENV{PETSC_DIR}/$ENV{PETSC_ARCH}/")
+set(NETCDF_DIR_F90 "$ENV{PETSC_DIR}/$ENV{PETSC_ARCH}/")
