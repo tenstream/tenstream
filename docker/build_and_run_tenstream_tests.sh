7f131e0ffb4ae7d29f682f70c659d446e4e2b69f
Fabian.Jakub <fabian@jakub.com>
2019-08-06 23:40:31 +0200

    add ctest -V -R $test to the gitlabCI script to run failed tests again with verbose flags

--- docker/build_and_run_tenstream_tests.sh
+++ docker/build_and_run_tenstream_tests.sh
@@ -14,4 +14,11 @@ make -j all || exit
 #bin/createLUT 3_10 || exit
 #bin/createLUT 8_10 || exit
 #bin/createLUT rectilinear_wedge_5_8 || exit
-make check || exit
+make check | tee ctest_log
+FAILED_TESTS=$(grep '...\*\*\*Failed' ctest_log | awk {'print $4'})
+for failed_test in $FAILED_TESTS
+do
+  echo "Re-Running verbosely failed test $failed_test"
+  ctest -V -R $failed_test
+done
+[ -z "$FAILED_TESTS" ] && (echo "Tests seem OK") || (echo "Some Tests failed"; exit)
