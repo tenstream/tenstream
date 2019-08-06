c06156305f71e22543c21977ee30ab8277bafba1
Fabian Jakub <fabian@jakub.com>
2019-08-07 01:03:33 +0200

    Update build_and_run_tenstream_tests.sh

--- docker/build_and_run_tenstream_tests.sh
+++ docker/build_and_run_tenstream_tests.sh
@@ -7,10 +7,10 @@ cd $WORKDIR/TenstreamLUT_cpy
 cp -v -rns $WORKDIR/TenstreamLUT/*.mmap4 $WORKDIR/TenstreamLUT_cpy
 echo "-lut_basename $(pwd)/LUT" >> $HOME/.petscrc
 
-cd $WORKDIR/tenstream || exit
+cd $WORKDIR/tenstream || exit 1
 rm -rf $WORKDIR/tenstream/build
 mkdir build && cd build && cmake .. -DSYST=$SYST -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DNETCDF_DIR=$NETCDF_DIR -DCTEST_MPIRUN_FLAGS="--allow-run-as-root" || exit
-make -j all || exit
+make -j all || exit 2
 
 make check | tee ctest_log
 
@@ -20,4 +20,4 @@ do
   echo "Re-Running verbosely failed test $failed_test"
   ctest -V -R $failed_test
 done
-[ -z "$FAILED_TESTS" ] && (echo "Tests seem OK") || (echo "Some Tests failed"; exit)
+[ -z "$FAILED_TESTS" ] && (echo "Tests seem OK") || (echo "Some Tests failed"; exit 3)
