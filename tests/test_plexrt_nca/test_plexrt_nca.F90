820db7d274d0f2e60f1236da22a05324ee6336dc
Carolin.Klinger <carolin.klinger@physik.uni-muenchen.de>
2019-11-18 11:54:33 +0100

    added tests for nca

--- tests/test_plexrt_nca/test_plexrt_nca.F90
+++ tests/test_plexrt_nca/test_plexrt_nca.F90
@@ -28,12 +28,14 @@ implicit none
 contains
   @test(npes =[1])
   subroutine test_single_box_nca(this)
+  !testing for 3 different optical depth
   class (MpiTestMethod), intent(inout) :: this
     integer(mpiint) :: comm
     real(ireals), parameter :: dx1=1e3, dx2=dx1, dx3=dx1, dz=1e3
     real(ireals) :: atop, abot, a1, a2, a3, vol, hr
     real(ireals) :: base_info(7), side_info(3*5)
-    real(ireals), parameter :: kabs=1e-5
+    real(ireals) :: kabs(3), results(3), delta(3)
+    integer(iintegers) :: itest
 
     comm     = this%getMpiCommunicator()
 
@@ -45,33 +47,56 @@ contains
 
     vol = atop * dz
 
-    base_info = [        &
-      kabs,        & !kabs
-      kabs,        & !kabs_top
-      0._ireals,   & !Ldn_top
-      1._ireals,   & !Btop
-      kabs,        & !kabs_bot
-      0._ireals,   & !Lup_bot
-      1._ireals    & !Bbot
-      ]
-    side_info(1:5) = [   &
-      kabs,        & !kabs
-      0._ireals,   & !Ldn_top
-      0._ireals,   & !Lup_top
-      0._ireals,   & !Ldn_bot
-      0._ireals    & !Lup_bot
-      ]
-
-    side_info( 6:10) = side_info(1:5)
-    side_info(11:15) = side_info(1:5)
-
-    call plexrt_nca_init(comm)
-
-    call plexrt_nca (dx1, dx2, dx3, &
-      dz, atop, abot, a1, a2, a3, vol, &
-      base_info, side_info, hr)
-
-    @assertEqual(-9.9e-5_ireals, hr, 1e-6_ireals, 'NCA Heating Test 1 false')
+    kabs = [    &
+         1.e-5, &
+         5.e-4, &
+         1.e2   &
+         ]
+    
+    results = [                &
+         -9.9e-5_ireals,       &
+         -0.4382770e-2_ireals, &
+         -0.2198385e-1_ireals  &
+         ]
+    
+    delta = [          &
+         1.e-6_ireals, &
+         1.e-3_ireals, &
+         1.e-2_ireals  &
+         ] 
+    
+    do itest=1,3
+       
+       base_info = [     &
+            kabs(itest), & !kabs
+            kabs(1),     & !kabs_top
+            0._ireals,   & !Ldn_top
+            1._ireals,   & !Btop
+            kabs(1),     & !kabs_bot
+            0._ireals,   & !Lup_bot
+            1._ireals    & !Bbot
+            ]
+       side_info(1:5) = [ &
+            kabs(1),      & !kabs
+            0._ireals,    & !Ldn_top
+            0._ireals,    & !Lup_top
+            0._ireals,    & !Ldn_bot
+            0._ireals     & !Lup_bot
+            ]
+       
+       side_info( 6:10) = side_info(1:5)
+       side_info(11:15) = side_info(1:5)
+       
+       call plexrt_nca_init(comm)
+       
+       call plexrt_nca (dx1, dx2, dx3, &
+            dz, atop, abot, a1, a2, a3, vol, &
+            base_info, side_info, hr)
+       
+       @assertEqual(results(itest), hr, delta(itest), 'NCA Heating Test 1')
+       
+    enddo
+ 
   end subroutine
 
   @test(npes =[1])
@@ -81,7 +106,7 @@ contains
     real(ireals), parameter :: dx1=1e3, dx2=dx1, dx3=dx1, dz=1e3
     real(ireals) :: atop, abot, a1, a2, a3, vol, hr
     real(ireals) :: base_info(7), side_info(3*5)
-    real(ireals), parameter :: kabs=100
+    real(ireals), parameter :: kabs=0.001
 
     comm     = this%getMpiCommunicator()
 
@@ -93,23 +118,24 @@ contains
 
     vol = atop * dz
 
-    base_info = [        &
-      kabs,        & !kabs
-      0._ireals,   & !kabs_top
-      10._ireals,   & !Ldn_top
-      10._ireals/pi,   & !Btop
-      0._ireals,   & !kabs_bot
-      10._ireals,   & !Lup_bot
-      10._ireals/pi    & !Bbot
+    base_info = [       &
+      kabs,             & !kabs
+      kabs,             & !kabs_top
+      214.14_ireals,    & !Ldn_top
+      318.91_ireals/pi, & !Btop
+      kabs,             & !kabs_bot
+      372.23_ireals,    & !Lup_bot
+      350.12_ireals/pi  & !Bbot
       ]
-    side_info(1:5) = [   &
-      kabs,        & !kabs
-     10._ireals,   & !Ldn_top
-     10._ireals,   & !Lup_top
-     10._ireals,   & !Ldn_bot
-     10._ireals    & !Lup_bot
+    side_info(1:5) = [  &
+      kabs,             & !kabs
+      214.14_ireals,    & !Ldn_top
+      339.96_ireals,    & !Lup_top
+      300.24_ireals,    & !Ldn_bot
+      372.23_ireals     & !Lup_bot
       ]
 
+    
     side_info( 6:10) = side_info(1:5)
     side_info(11:15) = side_info(1:5)
 
@@ -119,38 +145,12 @@ contains
       dz, atop, abot, a1, a2, a3, vol, &
       base_info, side_info, hr)
 
-    @assertEqual(0._ireals, hr, 1e-6_ireals)
+    !check if NCA result is within 15% of 1D
+    @assertEqual(-0.053829, hr, 0.053829*0.15)
 
-    base_info = [        &
-      kabs,        & !kabs
-      0._ireals,   & !kabs_top
-     10._ireals,   & !Ldn_top
-     10._ireals/pi,   & !Btop
-      0._ireals,   & !kabs_bot
-     20._ireals,   & !Lup_bot
-     20._ireals/pi    & !Bbot
-      ]
-    side_info(1:5) = [   &
-      kabs,        & !kabs
-     10._ireals,   & !Ldn_top
-     10._ireals,   & !Lup_top
-     20._ireals,   & !Ldn_bot
-     20._ireals    & !Lup_bot
-      ]
-
-    side_info( 6:10) = side_info(1:5)
-    side_info(11:15) = side_info(1:5)
-
-    call plexrt_nca_init(comm)
-
-    call plexrt_nca (dx1, dx2, dx3, &
-      dz, atop, abot, a1, a2, a3, vol, &
-      base_info, side_info, hr)
-
-    !@assertEqual(0._ireals, hr, 1e-6_ireals, 'varying fluxes')
   end subroutine
 
-  @test(npes =[2])
+  @test(npes =[1,2,3])
   subroutine test_nca_dmplex_interface(this)
   class (MpiTestMethod), intent(inout) :: this
     integer(mpiint) :: myid, numnodes, comm, ierr
@@ -161,7 +161,8 @@ contains
 
     type(tDM) :: dm2d_serial, dm2d, dm3d
     real(ireals) :: hhl(Nz)
-
+    real(ireals) :: hr_1d, hr
+    
     class(t_plex_solver), allocatable :: solver
     type(t_plexgrid), allocatable :: plex
     integer(iintegers), allocatable :: zindex(:)
@@ -198,7 +199,9 @@ contains
 
     call VecGetArrayF90(solver%kabs, xv, ierr); call CHKERR(ierr)
     xxv(1:Nz-1, 1:Ncol) => xv
-    if(myid.eq.0) xxv(Nz/2,:) = 10
+    !if(myid.eq.0) &
+    xxv(Nz/2,:) = 100
+  
     nullify(xxv)
     call VecRestoreArrayF90(solver%kabs, xv, ierr); call CHKERR(ierr)
 
@@ -224,7 +227,14 @@ contains
         print *, ''
         print *, 'Averages on rank'//itoa(myid)
         do k = 1, ubound(abso,1)
-        print *, k, meanval(edn(k,:)), meanval(eup(k,:)), meanval(abso(k,:))
+           print *, k, meanval(edn(k,:)), meanval(eup(k,:)), meanval(abso(k,:))
+           if(k.eq.Nz/2) then
+              hr_1d=meanval(edn(k,:))-meanval(edn(k+1,:))+meanval(eup(k+1,:))-meanval(eup(k,:))
+              hr= meanval(abso(k,:))
+              print *, k, hr_1d, hr
+              !check if NCA (@kabs=100) result is within 15% of 1D
+              @assertEqual(hr_1d, hr, (-1)*hr_1d*0.15)
+           endif
         enddo
         print *, k, meanval(edn(k,:)), meanval(eup(k,:))
       endif
