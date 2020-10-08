bd85abc3320501e98c7287a1a77ba92cccbd6ed3
Fabian.Jakub <fabian@jakub.com>
2020-10-08 04:11:10 +0200

    remove unused import from tests/test_plexrt_fish/test_plexrt_fish.F90

--- tests/test_plexrt_fish/test_plexrt_fish.F90
+++ tests/test_plexrt_fish/test_plexrt_fish.F90
@@ -1,40 +1,22 @@
 module test_plexrt_fish
 
   use m_examples_plex_ex_fish, only: plex_ex_fish
-!#include "petsc/finclude/petsc.h"
-!use petsc
-!
-use m_tenstream_options, only : read_commandline_options
 
-use m_data_parameters, only: &
-  & init_mpi_data_parameters, &
-  & finalize_mpi, &
-  & iintegers, ireals, mpiint, &
-  & zero, one
+  use m_tenstream_options, only : read_commandline_options
 
-use m_helper_functions, only: &
-  & CHKERR, &
-  & normalize_vec, &
-  & spherical_2_cartesian
+  use m_data_parameters, only: &
+    & init_mpi_data_parameters, &
+    & finalize_mpi, &
+    & iintegers, ireals, mpiint, &
+    & zero, one
 
-!use m_helper_functions, only: chkerr, cstr, itoa, &
-!  & approx, meanval, imp_allreduce_mean, meanvec, reverse
+  use m_helper_functions, only: &
+    & CHKERR, &
+    & spherical_2_cartesian
 
-!use m_plex_rt_base, only: t_plex_solver, allocate_plexrt_solver_from_commandline
-!use m_plex_grid, only: t_plexgrid, setup_plexgrid
-!use m_icon_plex_utils, only: create_2d_regular_plex, dmplex_2D_to_3D
-!
-!use m_plex_rt, only: &
-!  init_plex_rt_solver
-!
-!use m_plexrt_rrtmg, only: plexrt_rrtmg, destroy_plexrt_rrtmg
-!
-!use m_dyn_atm_to_rrtmg, only: t_tenstr_atm, setup_tenstr_atm, print_tenstr_atm, &
-!  & hydrostat_plev
+  use pfunit_mod
 
-use pfunit_mod
-
-implicit none
+  implicit none
 
 contains
 
