16038917b44699711f2a1e003b1c59150458c39a
Fabian.Jakub <fabian@jakub.com>
2020-10-26 02:08:38 +0100

    Revert "add -fallow-argument-mismatch to gcc default config"
    Because it somehow interferes with openmpi cmake finder
    
    This reverts commit c0aff68417868eb8cf28ab301a927e376b45b480.

--- config/gcc.cmake
+++ config/gcc.cmake
@@ -5,6 +5,6 @@
 message(STATUS "### USING GCC CONFIG ###")
 
 set(USER_C_FLAGS               "-W -Wall")
-set(USER_Fortran_FLAGS         "-W -Wall -fallow-argument-mismatch")
+set(USER_Fortran_FLAGS         "-W -Wall")
 set(USER_Fortran_FLAGS_RELEASE "-fno-backtrace -fno-range-check -O3 -mtune=native")
 set(USER_Fortran_FLAGS_DEBUG   "-ggdb -fbacktrace -fcheck=all -fbounds-check -pedantic -Wsurprising -Werror -Wuninitialized -ffpe-trap=invalid")
