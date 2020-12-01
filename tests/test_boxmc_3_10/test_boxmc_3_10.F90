b1ddcc3f4ea764c56b365523feac83d733d35787
HermannTheGreatMaster <hermann.boettcher@mail.de>
2020-12-01 17:21:37 +0100

    geometric correction working for  y-dir

--- tests/test_boxmc_3_10/test_boxmc_3_10.F90
+++ tests/test_boxmc_3_10/test_boxmc_3_10.F90
@@ -371,11 +371,11 @@ contains
     call check(S_target,T_target, S,T, msg=' test_boxmc_distorted_ns_cube_dir45_down_src2_case2')
   end subroutine
 
-  @test(npes =[1])
+  !@test(npes =[1])
   subroutine test_boxmc_distorted_cube_east_west_distortion_gomtc_corr(this)
     class (MpiTestMethod), intent(inout) :: this
     integer(iintegers) :: src
-    real(ireal_dp), allocatable :: verts(:)
+    real(ireal_dp), allocatable :: verts(:), verts_dtd(:)
     real(ireal_dp), parameter :: dx=1, dy=dx, dz=dx
     real(irealLUT) :: V(3)
     real(ireals) :: sundir(3)
@@ -383,13 +383,16 @@ contains
     bg  = [0e-0_ireal_dp/dz, 0._ireal_dp, 1._ireal_dp/2 ]
     S_target = zero
 
-    phi = 90; theta = 20
+    phi = 270; theta = 20
     src = 1
 
     !right side up
     call setup_default_unit_cube_geometry(dx, dy, dz, verts)
     call bmc_3_10%get_coeff(comm,bg,src,.True.,phi,theta,verts,S,T,S_tol,T_tol, inp_atol=atol, inp_rtol=rtol)
     V = real(T, irealLUT)
+
+    verts_dtd = verts
+    verts_dtd([6,12,18,24]) = verts_dtd([6,12,18,24]) + dz
 
     print *, 'vertices'
     print *, 'A', verts(1), verts(2), verts(3)
@@ -404,11 +407,9 @@ contains
     V = real(T, irealLUT)
     print *, 'regular not corrected', V(2), V(3), V(1)
     sundir = spherical_2_cartesian(real(phi, ireals), real(theta, ireals)) * [-one, -one, one]
-    call dir2dir3_coeff_correction_x_dir(V, verts, sundir, dz)
+    call dir2dir3_coeff_correction_x_dir(V, verts, verts_dtd, sundir)
     print *, 'regular corrected', V(2), V(3), V(1)
 
-
-    verts([6,12,18,24]) = verts([6,12,18,24]) + dz
     call bmc_3_10%get_coeff(comm,bg,src,.True.,phi,theta,verts,S,T_target,S_tol,T_tol, inp_atol=atol, inp_rtol=rtol)
     print *, 'distorted', T_target(2), T_target(3), T_target(1)
 
@@ -420,25 +421,92 @@ contains
   subroutine test_boxmc_distorted_cube_north_south_distortion_gomtc_corr(this)
     class (MpiTestMethod), intent(inout) :: this
     integer(iintegers) :: src
-    real(ireal_dp), allocatable :: verts(:)
+    real(ireal_dp), allocatable :: verts(:), verts_dtd(:)
     real(ireal_dp), parameter :: dx=1, dy=dx, dz=dx
     real(irealLUT) :: V(3)
+    real(ireals) :: sundir(3)
 
     bg  = [0e-0_ireal_dp/dz, 0._ireal_dp, 1._ireal_dp/2 ]
     S_target = zero
 
-    phi = 30; theta = 40
+    phi = 180; theta = 20
     src = 1
+
     !right side up
     call setup_default_unit_cube_geometry(dx, dy, dz, verts)
     call bmc_3_10%get_coeff(comm,bg,src,.True.,phi,theta,verts,S,T,S_tol,T_tol, inp_atol=atol, inp_rtol=rtol)
     V = real(T, irealLUT)
-    print *, 'regular not corrected', V
-    verts([9,12,21,24]) = verts([9,12,21,24]) + dz
-    call bmc_3_10%get_coeff(comm,bg,src,.True.,phi,theta,verts,S,T_target,S_tol,T_tol, inp_atol=atol, inp_rtol=rtol)
-    call dir2dir3_coeff_correction_y_dir(V, real(verts, ireals), spherical_2_cartesian(real(phi, ireals), real(theta, ireals)))
-    print *, 'regular', V
-    print *, 'distorted', T_target
+
+    verts_dtd = verts
+    verts_dtd([9,12,21,24]) = verts_dtd([9,12,21,24]) + dz
+
+    print *, 'vertices'
+    print *, 'A', verts(1), verts(2), verts(3)
+    print *, 'B', verts(4), verts(5), verts(6)
+    print *, 'C', verts(7), verts(8), verts(9)
+    print *, 'D', verts(10), verts(11), verts(12)
+    print *, 'E', verts(13), verts(14), verts(15)
+    print *, 'F', verts(16), verts(17), verts(18)
+    print *, 'G', verts(19), verts(20), verts(21)
+    print *, 'H', verts(22), verts(23), verts(24)
+
+    V = real(T, irealLUT)
+    print *, 'regular not corrected', V(2), V(3), V(1)
+    sundir = spherical_2_cartesian(real(phi, ireals), real(theta, ireals)) * [-one, -one, one]
+    call dir2dir3_coeff_correction_y_dir(V, verts, verts_dtd, sundir)
+    print *, 'regular corrected', V(2), V(3), V(1)
+
+    !verts([9,12,21,24]) = verts([9,12,21,24]) + dz
+    call bmc_3_10%get_coeff(comm,bg,src,.True.,phi,theta,verts_dtd,S,T_target,S_tol,T_tol, inp_atol=atol, inp_rtol=rtol)
+    print *, 'distorted', T_target(2), T_target(3), T_target(1)
+
+    !call check(S_target,T_target, S,T, msg=' test_boxmc_distorted_cube_dir45_up_src1')
+
+  end subroutine
+
+  @test(npes =[1])
+  subroutine test_boxmc_distorted_cube_distortion_gomtc_corr(this)
+    class (MpiTestMethod), intent(inout) :: this
+    integer(iintegers) :: src
+    real(ireal_dp), allocatable :: verts(:), verts_dtd(:)
+    real(ireal_dp), parameter :: dx=1, dy=dx, dz=dx
+    real(irealLUT) :: V(3)
+    real(ireals) :: sundir(3)
+
+    bg  = [0e-0_ireal_dp/dz, 0._ireal_dp, 1._ireal_dp/2 ]
+    S_target = zero
+
+    phi = 210; theta = 20
+    src = 1
+
+    !right side up
+    call setup_default_unit_cube_geometry(dx, dy, dz, verts)
+    call bmc_3_10%get_coeff(comm,bg,src,.True.,phi,theta,verts,S,T,S_tol,T_tol, inp_atol=atol, inp_rtol=rtol)
+    V = real(T, irealLUT)
+
+    verts_dtd = verts
+    verts_dtd([9,12,21,24]) = verts_dtd([9,12,21,24]) + dz
+
+    print *, 'vertices'
+    print *, 'A', verts(1), verts(2), verts(3)
+    print *, 'B', verts(4), verts(5), verts(6)
+    print *, 'C', verts(7), verts(8), verts(9)
+    print *, 'D', verts(10), verts(11), verts(12)
+    print *, 'E', verts(13), verts(14), verts(15)
+    print *, 'F', verts(16), verts(17), verts(18)
+    print *, 'G', verts(19), verts(20), verts(21)
+    print *, 'H', verts(22), verts(23), verts(24)
+
+    V = real(T, irealLUT)
+    print *, 'regular not corrected', V(2), V(3), V(1)
+    sundir = spherical_2_cartesian(real(phi, ireals), real(theta, ireals)) * [-one, -one, one]
+    call dir2dir3_coeff_correction_x_dir(V, verts, verts_dtd, sundir)
+    call dir2dir3_coeff_correction_y_dir(V, verts, verts_dtd, sundir)
+    print *, 'regular corrected', V(2), V(3), V(1)
+
+    !verts([9,12,21,24]) = verts([9,12,21,24]) + dz
+    call bmc_3_10%get_coeff(comm,bg,src,.True.,phi,theta,verts_dtd,S,T_target,S_tol,T_tol, inp_atol=atol, inp_rtol=rtol)
+    print *, 'distorted', T_target(2), T_target(3), T_target(1)
 
     !call check(S_target,T_target, S,T, msg=' test_boxmc_distorted_cube_dir45_up_src1')
 
