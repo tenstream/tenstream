b828033b9337070c63fcea9978f5f78a16242d76
Fabian.Jakub <fabian@jakub.com>
2021-02-25 16:45:46 +0100

    remove implicit none(external) warning for newer intel compilers because mpi and petsc dont conform to that

--- config/intel.cmake
+++ config/intel.cmake
@@ -1,5 +1,5 @@
 # Default intel compiler flags
 set(USER_C_FLAGS       "-W -Wall -mkl=sequential")
-set(USER_Fortran_FLAGS "-g -sox -no-wrap-margin -mkl=sequential -warn all")
+set(USER_Fortran_FLAGS "-g -sox -no-wrap-margin -mkl=sequential -warn all,noexternal")
 set(USER_Fortran_FLAGS_RELEASE "-mtune=native -O3 -ftz -pc64 -fp-model fast=2 -no-prec-sqrt -fast-transcendentals")
-set(USER_Fortran_FLAGS_DEBUG "-traceback -extend_source -g -fp-model strict -ftrapuv -warn all -warn errors -fpe0 -O2 -g -check all -check nopointers -check noarg_temp_created ")
+set(USER_Fortran_FLAGS_DEBUG "-traceback -extend_source -g -fp-model strict -ftrapuv -warn errors -fpe0 -O2 -g -check all -check nopointers -check noarg_temp_created ")
