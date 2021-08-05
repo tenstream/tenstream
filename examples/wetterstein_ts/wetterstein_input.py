31de56f158b3bbdd5a0558cf201b6a57a82eb081
HermannTheGreatMaster <hermann.boettcher@mail.de>
2021-08-05 15:41:55 +0200

    update wetterstein_input.py file to include lwc

--- examples/wetterstein_ts/wetterstein_input.py
+++ examples/wetterstein_ts/wetterstein_input.py
@@ -8,17 +8,21 @@ def load_srtm_wetterstein():
     """ Load elevation data from SRTM surrounding the Wetterstein massif """
     import netCDF4 as NC
     D = NC.Dataset('means_wetterstein.cdf')
-    elev = D['elevation'][255:511, 255:511]
+    #elev = D['elevation'][255:511, 255:511]
+    elev = D['elevation'][310:420, 310:500]
     D.close()
     return elev.T
 
-def create_var(varname, var, dim_prefix):
+def create_var(varname, var, dim_prefix, lcld):
     """ Write regridded data to netcdf file """
     import os
     import netCDF4 as NC
     from scipy.interpolate import interp1d
 
-    fname = 'input.nc'
+    fname = 'input'
+    if (lcld == True):
+        fname += '_cld'
+    fname += '.nc'
     if os.path.exists(fname):
         D = NC.Dataset(fname, 'a')
     else:
@@ -62,7 +66,20 @@ def interp_var(old_pressure_grid, var, new_pressure_grid):
    return new_var
 
 
-def create_srtm_input():
+def create_cld(wetterstein, hill, lay_coord, lcld):
+    from scipy.ndimage.filters import gaussian_filter as gf
+    max_heights_indice = np.argmax(wetterstein, axis=1)
+    lwc = np.zeros(lay_coord.shape)
+    if (lcld == True):
+        for y_index in range(wetterstein.shape[1]):
+            x_index = np.argmax(wetterstein[:,y_index])
+            z_index = np.argwhere(hill[:,:,::-1][x_index,y_index,:] > wetterstein[x_index,y_index] + 1.)[0,0]
+            lwc[x_index,y_index,z_index] = 10.
+            lwc = gf(lwc, [3,3,2], mode='wrap')
+    return lwc
+
+
+def create_srtm_input(lcld):
     """ Create Input for TenStream//rrtmg caluclations surrounding the Wetterstein massif """
     from scipy.ndimage.filters import gaussian_filter as gf
     from scipy.interpolate import interp1d
@@ -70,12 +87,18 @@ def create_srtm_input():
     # wetterstein = load_srtm_wetterstein(xmin=3000, xmax=3200, ymin=1100, ymax=1300)
     wetterstein = load_srtm_wetterstein()
 
-    # make sure that height values around the border are cyclic
-    wetterstein[:,0] = wetterstein[:,-1] = wetterstein[:,[0,-1]].mean(axis=1)
-    wetterstein[0,:] = wetterstein[-1,:] = wetterstein[[0,-1],:].mean(axis=0)
+    wetterstein = np.hstack((wetterstein, wetterstein[:,::-1]))
+    wetterstein = np.vstack((wetterstein, wetterstein[::-1,:]))
 
     # smooth topography
-    wetterstein = gf(wetterstein, 1.5, mode='wrap')
+    dx = 0.1
+    dy = 0.1
+    max_angle = np.max(np.degrees(np.arctan(np.gradient(wetterstein, dx, dy))))
+    print(max_angle)
+    #sigma = 1.1
+    #while max_angle > 35.:
+    #    wetterstein = gf(wetterstein, sigma, mode='wrap')
+    #    max_angle = np.max(np.degrees(np.arctan(np.gradient(wetterstein, dx, dy))))
 
     Nx, Ny = np.shape(wetterstein)
 
@@ -98,23 +121,25 @@ def create_srtm_input():
     hill = hill[:, :, ::-1]  # hhl now begins at TOA
     p_hill = pressure_by_height(hill)
 
-    kmax = np.argmax(p_hill > 100) # drop everything above 100hPa
+    kmax = np.argmax(p_hill > 300) # drop everything above 300hPa
     lev_coord = p_hill[:,:,kmax:]
 
-    create_var('plev', interp_var(p, afglus[:, 1], lev_coord), 'lev')
-    create_var('tlev', interp_var(p, afglus[:, 2], lev_coord), 'lev')
+    create_var('plev', interp_var(p, afglus[:, 1], lev_coord), 'lev', lcld)
+    create_var('tlev', interp_var(p, afglus[:, 2], lev_coord), 'lev', lcld)
+    lay_coord = (lev_coord[:, :, 1:] + lev_coord[:, :, :-1]) / 2
+    create_var('lwc', create_cld(wetterstein,hill,lay_coord, lcld), 'lay', lcld)
 
     if False:
-        lay_coord = (lev_coord[:, :, 1:] + lev_coord[:, :, :-1]) / 2
 
-        create_var('tlay',   interp_var(p, afglus[:, 2], lay_coord), 'lay')
-        create_var('air',    interp_var(p, afglus[:, 3], lay_coord), 'lay')
-        create_var('o3vmr',  interp_var(p, afglus[:, 4], lay_coord), 'lay')
-        create_var('o2vmr',  interp_var(p, afglus[:, 5], lay_coord), 'lay')
-        create_var('h2ovmr', interp_var(p, afglus[:, 6], lay_coord), 'lay')
-        create_var('co2vmr', interp_var(p, afglus[:, 7], lay_coord), 'lay')
-        create_var('n2ovmr', interp_var(p, afglus[:, 8], lay_coord), 'lay')
+        create_var('tlay',   interp_var(p, afglus[:, 2], lay_coord), 'lay', lcld)
+        create_var('air',    interp_var(p, afglus[:, 3], lay_coord), 'lay', lcld)
+        create_var('o3vmr',  interp_var(p, afglus[:, 4], lay_coord), 'lay', lcld)
+        create_var('o2vmr',  interp_var(p, afglus[:, 5], lay_coord), 'lay', lcld)
+        create_var('h2ovmr', interp_var(p, afglus[:, 6], lay_coord), 'lay', lcld)
+        create_var('co2vmr', interp_var(p, afglus[:, 7], lay_coord), 'lay', lcld)
+        create_var('n2ovmr', interp_var(p, afglus[:, 8], lay_coord), 'lay', lcld)
 
 
 if __name__ == '__main__':
-    create_srtm_input()
+    create_srtm_input(False)
+    create_srtm_input(True)
