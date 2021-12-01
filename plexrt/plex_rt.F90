b7e16499c828d85f364a3e37ae42b4c82a3bd837
Fabian Jakub <fabian@jakub.com>
2021-12-01 22:53:38 +0100

    use tauz instead of kabs in B_eff computations in plexrt

--- plexrt/plex_rt.F90
+++ plexrt/plex_rt.F90
@@ -2095,7 +2095,7 @@ module m_plex_rt
           integer(iintegers), pointer :: faces_of_cell(:)
           integer(iintegers) :: face_plex2bmc(5)
           integer(iintegers) :: diff_plex2bmc(8)
-          integer(iintegers) :: wedge_offset2, plck_offset
+          integer(iintegers) :: wedge_offset2, plck_offset, geom_offset
 
           integer(iintegers), allocatable :: incoming_offsets(:), outgoing_offsets(:)
           integer(iintegers) :: i, j, icell, icol, iface, idof, numDof
@@ -2103,7 +2103,7 @@ module m_plex_rt
 
           integer(iintegers), pointer :: xinoutdof(:)
           real(ireals), pointer :: xplanck(:)
-          real(ireals) :: emissivity, b0, b1, btop, bbot, bside, Beff
+          real(ireals) :: emissivity, b0, b1, btop, bbot, bside, Beff, dz, tauz
 
           call ISGetIndicesF90(solver%IS_diff_in_out_dof, xinoutdof, ierr); call CHKERR(ierr)
           call VecGetArrayReadF90(plckVec, xplanck, ierr); call CHKERR(ierr)
@@ -2126,8 +2126,13 @@ module m_plex_rt
             b0 = xplanck(i1+plck_offset)* pi ! top planck value
             call PetscSectionGetFieldOffset(plckSection, faces_of_cell(2), i0, plck_offset, ierr); call CHKERR(ierr)
             b1 = xplanck(i1+plck_offset)* pi ! bot planck value
-            call B_eff(b1, b0, xkabs(i1+icell), btop)
-            call B_eff(b0, b1, xkabs(i1+icell), bbot)
+
+            call PetscSectionGetFieldOffset(geomSection, icell, i3, geom_offset, ierr); call CHKERR(ierr)
+            dz = geoms(i1+geom_offset)
+            tauz = xkabs(i1+icell)*dz
+
+            call B_eff(b1, b0, tauz, btop)
+            call B_eff(b0, b1, tauz, bbot)
             bside = (btop+bbot)/2
 
             i = 1
