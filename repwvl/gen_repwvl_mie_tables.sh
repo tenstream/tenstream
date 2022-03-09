b48938b7025cbfa78463bb65762a4d3c084fa225
Fabian.Jakub <fabian@jakub.com>
2022-03-09 14:08:05 +0100

    add script to generate mie tables for a particular repwvl set

---
+++ repwvl/gen_repwvl_mie_tables.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -euo pipefail
+
+SOLARDB=${1:-missing 1st argument}
+THERMALDB=${2:-missing 2nd argument}
+
+if [ ! -e "$SOLARDB"   ]; then echo "Solar database file (arg 1) does not exist: $SOLARDB"; exit 1; fi
+if [ ! -e "$THERMALDB" ]; then echo "Thermal database file (arg 2) does not exist: $THERMALDB"; exit 1; fi
+
+WVLVAR=ChosenWvls
+
+ALL_WVL=
+for DB in $SOLARDB $THERMALDB; do
+  WVL=$(python -c "import xarray as xr; D=xr.open_dataset('$DB'); print(' '.join(map(str, D['$WVLVAR'].data)))")
+  ALL_WVL="$WVL $ALL_WVL"
+done
+
+echo "Combined wavelengths are $ALL_WVL"
+
+REFF_WC=$(seq -s " " 2 .1 40)
+REFF_IC=$(seq -s " " 20 .1 140)
+
+WC=mie.wc.table.nc
+if [ ! -e $WC ]; then
+echo python gen_mie_tables.py $WC -p water \
+  --wvls $ALL_WVL \
+  --reffs $REFF_WC \
+  -v -m $WORK/lib/libRadtran/bin/mie
+else
+  echo "Skipping computation of $WC because file already exists"
+fi
+
+IC=mie.ic.table.nc
+if [ ! -e $IC ]; then
+python gen_mie_tables.py $IC -p ice \
+  --wvls $ALL_WVL \
+  --reffs $REFF_IC \
+  -v -m $WORK/lib/libRadtran/bin/mie
+else
+  echo "Skipping computation of $IC because file already exists"
+fi
+
+echo done
