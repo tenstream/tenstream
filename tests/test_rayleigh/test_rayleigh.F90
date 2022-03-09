f49df10f0a1041aa68aba179475ef17e5eaf0773
Fabian.Jakub <fabian@jakub.com>
2022-03-09 14:35:27 +0100

    rayleigh return NAN where simple bodhaine fails for small lambdas

--- tests/test_rayleigh/test_rayleigh.F90
+++ tests/test_rayleigh/test_rayleigh.F90
@@ -1,8 +1,10 @@
 module test_rayleigh
 
   use m_data_parameters, only:  &
+    & finalize_mpi,             &
+    & init_mpi_data_parameters, &
     & iintegers,                &
-    & ireals, &
+    & ireals,                   &
     & mpiint
 
   use m_rayleigh, only: &
@@ -17,11 +19,13 @@ contains
   @before
   subroutine setup(this)
     class(MpiTestMethod), intent(inout) :: this
+    call init_mpi_data_parameters(this%getMpiCommunicator())
   end subroutine setup
 
   @after
   subroutine teardown(this)
     class(MpiTestMethod), intent(inout) :: this
+    call finalize_mpi(this%getMpiCommunicator(), lfinalize_mpi=.false., lfinalize_petsc=.true.)
   end subroutine teardown
 
   @test(npes = [1])
@@ -33,20 +37,30 @@ contains
     integer(iintegers) :: i
     integer(mpiint) :: ierr
 
-    do i = 200, 10000, 100
+    do i = 100, 5000, 100
       wvl = real(i) * 1e-3_ireals
 
       call rayleigh(wvl, ksca, ierr)
-      @assertEqual(0, ierr, 'Should be valid output')
+      if (ierr .eq. 0) then
+        @assertTrue(ksca.gt.0, 'should be positive')
+      end if
 
       call rayleigh(wvl, co2, ksca2, ierr)
-      @assertEqual(0, ierr, 'Should be valid output')
+      if (ierr .eq. 0) then
+        @assertTrue(ksca2.gt.0, 'should be positive')
+      end if
 
       print *, 'wvl', wvl, '[mu]', ' bodhaine', ksca, 'bodhaine_co2', ksca2
     end do
 
     call rayleigh(.1_ireals, ksca, ierr)
     @assertTrue(ierr.ne.0, 'Should throw an error for small wavelengths')
+
+    call rayleigh(.5, ksca, ierr)
+    @assertEqual(6.66140033E-27_ireals, ksca)
+
+    call rayleigh(.5, co2, ksca2, ierr)
+    @assertEqual(6.66123701E-27_ireals, ksca2)
   end subroutine
 
 end module
