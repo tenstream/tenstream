6aa16357fcc6b406e6cf6c6454045f3405fd48c9
Fabian.Jakub <fabian@jakub.com>
2022-03-09 09:28:07 +0100

    patch fprettify to ignore pfunit macros

--- maint/fprettify.sh
+++ maint/fprettify.sh
@@ -26,6 +26,7 @@ PYTHON=$(which "python3")
 BIN=$PROJECT_ROOT/external/fprettify/fprettify.py
 if [[ ! -e $BIN ]]; then
   git clone https://github.com/pseewald/fprettify.git $(dirname $BIN)
+  sed -i 's#COMMENT_LINE_STR = r"^!"#COMMENT_LINE_STR = r"^!|^@"#' $(dirname $BIN)/fprettify/fparse_utils.py
 fi
 DIFF=false
 DRYRUN=false
