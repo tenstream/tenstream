b5c46845a46b626c1efb40c9312f933496a47300
Fabian.Jakub <fabian@jakub.com>
2022-03-28 10:04:35 +0200

    fix use of CHKERRQ to -> call CHKERR

--- plexrt/plex_grid.F90
+++ plexrt/plex_grid.F90
@@ -304,7 +304,7 @@ contains
       call get_petsc_opt(PETSC_NULL_CHARACTER, "-check_height_for_consistency", ldocheck, lflg, ierr); call CHKERR(ierr)
       if (.not. ldocheck) return
 
-      call DMGetSection(plex%geom_dm, geom_section, ierr); CHKERRQ(ierr)
+      call DMGetSection(plex%geom_dm, geom_section, ierr); call CHKERR(ierr)
       call VecGetArrayReadF90(plex%geomVec, xgeoms, ierr); call CHKERR(ierr)
 
       do icell = plex%cStart, plex%cEnd - 1
