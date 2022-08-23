4eb2153a399e207e572b06d2be92510b13cd9fc8
Fabian.Jakub <fabian@jakub.com>
2022-08-23 12:00:16 +0200

    add verbose atmosphere output to ex_pprts_specint_buildings example

--- examples/pprts/ex_pprts_specint_buildings.F90
+++ examples/pprts/ex_pprts_specint_buildings.F90
@@ -8,7 +8,7 @@ program main
     & init_mpi_data_parameters, finalize_mpi
   use m_buildings, only: t_pprts_buildings
   use m_examples_pprts_specint_buildings, only: ex_pprts_specint_buildings
-  use m_helper_functions, only: CHKERR, toStr, get_petsc_opt
+  use m_helper_functions, only: CHKERR, toStr, get_petsc_opt, meanval
   use m_netcdfio, only: ncwrite
   use m_tenstream_options, only: read_commandline_options
 
@@ -27,6 +27,7 @@ program main
   character(len=default_str_len) :: groups(2), specint
   logical :: lflg, lverbose, lrayli_opts, lsolar, lthermal, lfile_exists, lhave_outfile, lbuildings
   integer(mpiint) :: cid, comm, myid, numnodes, ierr
+  integer(iintegers) :: k
 
   call mpi_init(ierr)
   comm = mpi_comm_world
@@ -175,6 +176,25 @@ program main
       call mpi_barrier(comm, ierr); call CHKERR(ierr)
     end if
   end if
+
+  if (lverbose .and. myid.eq.0) then
+    print *,''
+    if(lsolar) then
+      print *,'          k   edir             edn              eup             abso'
+      do k=lbound(gabso,1), ubound(gabso,1)
+        print *, k, meanval(gedir(k,:,:)), meanval(gedn(k,:,:)), meanval(geup(k,:,:)), meanval(gabso(k,:,:))
+      enddo
+      k = ubound(gedn,1)
+      print *, k, meanval(gedn(k,:,:)), meanval(geup(k,:,:))
+    else
+      print *,'          k   edn              eup             abso'
+      do k=lbound(gabso,1), ubound(gabso,1)
+        print *, k, meanval(gedn(k,:,:)), meanval(geup(k,:,:)), meanval(gabso(k,:,:))
+      enddo
+      k = ubound(gedn,1)
+      print *, k, meanval(gedn(k,:,:)), meanval(geup(k,:,:))
+    endif
+  endif
 
   call finalize_mpi(ierr, .true., .true.)
 end program
