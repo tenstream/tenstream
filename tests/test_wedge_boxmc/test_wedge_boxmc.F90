3792a1d7f4e476936aeab2af97f728f5cb4b2ab6
Fabian.Jakub <fabian@jakub.com>
2023-05-02 15:17:22 +0200

    properly finish petsc in test_wedge_boxmc

--- tests/test_wedge_boxmc/test_wedge_boxmc.F90
+++ tests/test_wedge_boxmc/test_wedge_boxmc.F90
@@ -57,6 +57,10 @@ contains
   @after
   subroutine teardown(this)
     class(MpiTestMethod), intent(inout) :: this
+    logical :: lpetsc_is_initialized
+    integer(mpiint) :: ierr
+    call PetscInitialized(lpetsc_is_initialized, ierr)
+    if (lpetsc_is_initialized) call PetscFinalize(ierr)
     if (myid .eq. 0) print *, 'Finishing boxmc tests module'
   end subroutine teardown
 
